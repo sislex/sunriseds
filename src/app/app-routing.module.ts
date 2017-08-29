ec15dcd33150c0a7d87c73dc7639914d5b4cc156
Sergei <3Hopkins@mail.ru>
2017-08-29 17:36:20 +0300

    Added all components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, PreloadAllModules} from '@angular/router';
+import {ContactsComponent} from './components/contacts/contacts.component';
+import {HomeComponent} from './components/home/home.component';
+import {AboutComponent} from './components/about/about.component';
+import {TeamComponent} from './components/team/team.component';
+import {TeamDetailsComponent} from './components/team-details/team-details.component';
+import {ProjectsComponent} from "./components/projects/projects.component";
+
+const ROUTES = [
+    {path: '', component: HomeComponent},
+    {path: 'projects', component: ProjectsComponent},
+    {path: 'contacts', component: ContactsComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'team', component: TeamComponent},
+    {path: 'team-details', component: TeamDetailsComponent},
+    {path: '**', component: HomeComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(ROUTES, {useHash: false, preloadingStrategy: PreloadAllModules})],
+    exports: [RouterModule],
+    providers: []
+})
+export class AppRoutingModule {
+}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ContentComponent } from './components/content/content.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/header/header.component';
+import {ContentComponent} from './components/content/content.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {MainMenuComponent} from './components/main-menu/main-menu.component';
+import {ContactsComponent} from './components/contacts/contacts.component';
+import {HomeComponent} from './components/home/home.component';
+import {AppRoutingModule} from './app-routing.module';
+import { AboutComponent } from './components/about/about.component';
+import { TeamComponent } from './components/team/team.component';
+import { TeamDetailsComponent } from './components/team-details/team-details.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { PartnersComponent } from './components/partners/partners.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    ContentComponent,
-    FooterComponent,
-    MainMenuComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        ContentComponent,
+        FooterComponent,
+        MainMenuComponent,
+        ContactsComponent,
+        HomeComponent,
+        AboutComponent,
+        TeamComponent,
+        TeamDetailsComponent,
+        ProjectsComponent,
+        PartnersComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
