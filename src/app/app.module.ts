decd5be53aa92d4fe5a55f0b97d83fd6a6bdbda6
Sergei <3Hopkins@mail.ru>
2017-09-01 17:32:22 +0300

    Delite component "partners"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,6 @@ import { AboutComponent } from './components/about/about.component';
 import { TeamComponent } from './components/team/team.component';
 import { TeamDetailsComponent } from './components/team-details/team-details.component';
 import { ProjectsComponent } from './components/projects/projects.component';
-import { PartnersComponent } from './components/partners/partners.component';
 import {UsersService} from './services/users.service';
 import {RequestsService} from './services/requests.service';
 import {HttpClientModule} from '@angular/common/http';
@@ -34,7 +33,6 @@ import {StorageService} from './services/storage.service';
         TeamComponent,
         TeamDetailsComponent,
         ProjectsComponent,
-        PartnersComponent,
         AdminPanelComponent
     ],
     imports: [
